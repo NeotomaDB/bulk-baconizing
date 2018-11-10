b91a07d7e5f52f3f5bd4ccdec17e406273779099
Simon <simon.j.goring@gmail.com>
2018-11-09 16:24:08 -0800

    Had to convert the posterior output to a data.frame so it would work.  Also added the Rplots.pdf to the gitignore.

--- R/bacon_age_posts.R
+++ R/bacon_age_posts.R
@@ -1,45 +1,45 @@
 #' @title Get age posteriors
 #' @description Using the output files from Bacon get the full posterior at depths.
 #' @param handle The site handle.
-#' 
+#'
 
 bacon_age_posts <- function(handle)
-{ 
-  
-  out_files <- list.files(paste0('Cores/', handle), 
+{
+
+  out_files <- list.files(paste0('Cores/', handle),
                           pattern = ".out$",
                           full.names = TRUE)
-  
-  depth <- readr::read_csv(paste0('Cores/', handle, '/', handle, '_depths.txt'), 
-                           col_names = FALSE) %>% 
+
+  depth <- readr::read_csv(paste0('Cores/', handle, '/', handle, '_depths.txt'),
+                           col_names = FALSE) %>%
     as.data.frame()
-  
-  bacon_settings <- readr::read_csv(paste0('Cores/', handle, '/', handle, '_settings.txt'), 
+
+  bacon_settings <- readr::read_csv(paste0('Cores/', handle, '/', handle, '_settings.txt'),
                                     col_names = FALSE, comment = '#') %>% as.data.frame
-  
-  
+
+
   if (length(out_files) > 1) {
     message('Multiple Bacon output files exist.')
   }
-  
+
   for (i in 1:length(out_files)) {
-    
+
     outer <- readr::read_delim(out_files[i], col_names = FALSE,delim = ' ') %>% as.data.frame
-    
+
     # We can do this match because we know how Bacon writes out files.
-    sections <- stringr::str_match(out_files[i], '(?:_)([0-9]*)\\.')[2] %>% 
+    sections <- stringr::str_match(out_files[i], '(?:_)([0-9]*)\\.')[2] %>%
       as.numeric()
-    
+
     if (ncol(outer) == (sections + 3)) {
       priors <- matrix(NA, nrow = nrow(depth), ncol = nrow(outer))
-      
+
       for(j in 1:nrow(outer)) {
         x <- seq(bacon_settings[1,1], bacon_settings[2,1], length.out = sections + 1)
         y <- c(outer[j,1], outer[j,1] + cumsum((diff(x) * outer[j,2:(ncol(outer) - 2)]) %>% as.numeric))
         priors[,j] <- approx(x, y, xout = depth %>% unlist())$y
       }
-      readr::write_csv(paste0('Cores/', handle, '/', handle, '_', sections, '_priorout.csv'), x = priors)
+      readr::write_csv(paste0('Cores/', handle, '/', handle, '_', sections, '_priorout.csv'), x = as.data.frame(priors))
     }
-    
+
   }
 }

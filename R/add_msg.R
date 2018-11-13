c8bc23a1f573939091b5096aa9ce16ee35b263d6
Simon <simon.j.goring@gmail.com>
2018-11-13 14:03:21 -0800

    Updated agg_msg so that it can accept a single element or a vector of elements.

--- R/add_msg.R
+++ R/add_msg.R
@@ -1,16 +1,24 @@
 add_msg <- function(x, msg) {
-  
-  if(stringr::str_detect(string = x, pattern = msg)) {
-    out <- x
-  } else {
-    if (nchar(x) == 1) {
-      x <- ''
-      pre <- ''
+  message <- function(x, msg) {
+    if(stringr::str_detect(string = x, pattern = msg)) {
+      out <- x
     } else {
-      pre <- '; '
+      if (nchar(x) == 1) {
+        x <- ''
+        pre <- ''
+      } else {
+        pre <- '; '
+      }
+      out <- paste0(x, pre, msg)
     }
-    out <- paste0(x, pre, msg)
+    return(out)
   }
   
+  if (length(x) > 1) {
+    out <- sapply(x, function(x) add_msg(x, msg))
+  } else {
+    out <- message(x, msg)
+  }
+    
   return(out)
 }

52b14ab32bc394a2c1c6c8a303093724d748ae49
Simon <simon.j.goring@gmail.com>
2018-11-19 15:09:48 -0800

    Using linting to fix files to a better coding standard.  Also suppressing download messages.

--- R/load_downloads.R
+++ R/load_downloads.R
@@ -1,23 +1,23 @@
-#' @title Load download records to from/file
-#' @description Checks for an existing pollen file with the right version number and loads it in.  If the file does not exist it will create it.
-#' @param dl A \code{neotoma} dataset or dataset_list object.
-#' @param path The path to check, defaults to the current directory.
-#' @param version A version number.  Default is NULL.
-#' @param setup Can automatically re-run.
-#' @returns A \code{neotoma} \code{download_list} object.
+#" @title Load download records to from/file
+#" @description Checks for an existing pollen file with the right version number and loads it in.  If the file does not exist it will create it.
+#" @param dl A \code{neotoma} dataset or dataset_list object.
+#" @param path The path to check, defaults to the current directory.
+#" @param version A version number.  Default is NULL.
+#" @param setup Can automatically re-run.
+#" @returns A \code{neotoma} \code{download_list} object.
 
-load_downloads <- function(dl, path = './', version = NULL, setup = FALSE) {
-  
-  if (file.exists(paste0(path, 'data/pollen_v', version, '.rds'))) {
+load_downloads <- function(dl, path = "./", version = NULL, setup = FALSE) {
+
+  if (file.exists(paste0(path, "data/pollen_v", version, ".rds"))) {
     if (setup == TRUE) {
-      pol <- get_download(dl)
-      saveRDS(pol, paste0(path, 'data/pollen_v', version, '.rds'))
+      pol <- suppressMessages(neotoma::get_download(dl))
+      saveRDS(pol, paste0(path, "data/pollen_v", version, ".rds"))
     }
-    pol <- readRDS(paste0(path, 'data/pollen_v', version, '.rds'))
+    pol <- readRDS(paste0(path, "data/pollen_v", version, ".rds"))
   } else {
-    pol <- get_download(dl)
-    saveRDS(pol, paste0(path, 'data/pollen_v', version, '.rds'))
+    pol <- suppressMessages(neotoma::get_download(dl))
+    saveRDS(pol, paste0(path, "data/pollen_v", version, ".rds"))
   }
-  
+
   return(pol)
 }
